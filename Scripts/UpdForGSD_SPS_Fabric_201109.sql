49e8728166641112d8be67a09c72a546cb594462
Sam Sum (ISD CKL) <sam_sum@crystalgroup.com>
2019-01-24 22:53:12 +0000

    Original ChangesetId: 5425, Committer: TSTHKDOMAIN\lwsyli2, CreationDate: 02/09/2011 11:33:06 AM, Comment:
    git-tfs-id: [http://tsttfs01:8080/tfs/DefaultCollection]$/GSD;C9064

--- Scripts/UpdForGSD_SPS_Fabric_201109.sql
+++ Scripts/UpdForGSD_SPS_Fabric_201109.sql
@@ -1,4 +1,9 @@
+/**
+处理SPS Status Report的时候发现Remarks不能显示中文，把此字段转换成nvarchar的类型后再试，
+发现ETL从SAP里面取到的数据中文已经变成#号，目前没办法通过ELT来解决这个问题，所以暂时没更新。
+	-- Rocky 02 Sep 2011
 print 'alter SPS_Fabric set column Remarks type to nvarchar(80)'
 alter table SPS_Fabric_Hist alter column Remarks nvarchar(80)
 alter table SPS_Fabric_Tmp alter column Remarks nvarchar(80)
 alter table SPS_Fabric alter column Remarks nvarchar(80)
+**/
