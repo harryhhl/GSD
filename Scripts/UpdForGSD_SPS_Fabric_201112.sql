ff65a1e287f80040ef5f09208615e2d20b88dbdb
Sam Sum (ISD CKL) <sam_sum@crystalgroup.com>
2019-01-24 22:53:24 +0000

    Original ChangesetId: 5881, Committer: TSTHKDOMAIN\lwsyli2, CreationDate: 26/12/2011 3:45:44 PM, Comment:
    git-tfs-id: [http://tsttfs01:8080/tfs/DefaultCollection]$/GSD;C9066

---
+++ Scripts/UpdForGSD_SPS_Fabric_201112.sql
@@ -0,0 +1,6 @@
+alter table SPS_Fabric alter column Special_ACC varchar(20)
+alter table SPS_Fabric_Hist alter column Special_ACC varchar(20)
+alter table SPS_Fabric_Tmp alter column Special_ACC varchar(20)
+
+
+
